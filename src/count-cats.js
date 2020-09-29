c60013e593e03b5c455dfc44e355a10ad548bbdd
unknown <samotv82@gmail.com>
2020-09-30 00:04:24 +0300

    feat: task cats was added

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,12 +1,35 @@
 module.exports = function countCats(arr) {
-  throw 'Not implemented';
+ 
   let sum=0;
   let arr1 =[];
   for( let i=0; i<arr.length; i++){
 arr1=arr[i];
+if (arr[i] == '^^') 
+  sum++;
 for (let j=0; j<arr1.length; j++){
   if (arr1[j] != '^^') continue;
   sum++;
 }
   }
+  return sum;
 };
+/*module.exports = function countCats(arr) {
+  let sum=0;
+  
+  for(let i=0; i<rows; i++)
+  arr[i]=new Array;
+  {
+   if (arr[i] == '^^') 
+  sum++;
+//arr1=arr[i];
+for (let j=0; j<columns; j++)
+arr[j]= new Array;
+{
+  if (arr[j] == '^^') 
+  sum++;
+  //if (arr [i][j]=='^','^')
+  //sum++;
+}
+  }
+  return sum;
+};*/
